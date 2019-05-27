487a25087ff68f027e8a9db0f11cbb61bd44ca54
Harpreet Singh <harpreetsingh@Harpreets-MacBook-Pro.local>
2019-05-26 21:54:43 -0700

    Initial commit + smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+	// Store candidate
+	// Read candidate
+	// Constructor
+	string public candidate;
+
+	function Constructor () public {
+		candidate = "Candidate 1";
+
+	}
+
+}
